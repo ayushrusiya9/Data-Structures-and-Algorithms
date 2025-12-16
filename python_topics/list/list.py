b9c33dd2df17adc076574a4692172eb32760273e
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-12-16 13:00:47 +0530

    update

---
+++ python_topics/list/list.py
@@ -0,0 +1,9 @@
+l = [1,2,3,4]
+
+max_value = l[0]
+
+for i in l:
+    if max_value < i:
+        max_value = i
+
+print(max_value)
---
+++ python_topics/tuple/tuple.py
@@ -0,0 +1,9 @@
+t = (1,2,4,5)
+
+min_value = t[0]
+
+for i in t:
+    if min_value > i:
+        min_value = i
+
+print(min_value)
