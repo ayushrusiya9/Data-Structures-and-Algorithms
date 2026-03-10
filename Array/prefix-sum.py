390aa2f5c7cce88c27fdce997a3b6616a00b8750
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-03-10 22:18:58 +0530

    new que with solutiom

--- Array/prefix-sum.py
+++ Array/prefix-sum.py
@@ -1,4 +1,8 @@
-def prefix_sum():
-    """
-    prefix 
-    """
+def prefix_sum(arr):
+    prefix_sum = [0] * len(arr)
+    prefix_sum[0] = arr[0]
+    for i in range(1, len(arr)):
+        prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+    return prefix_sum
+
+print(prefix_sum([1, 2, 3, 4, 5]))
