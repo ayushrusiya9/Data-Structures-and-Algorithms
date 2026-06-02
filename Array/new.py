98342e74c769419f3b3ad4f819a4e741dbc87025
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-06-02 22:33:51 +0530

    feat: optimize new que code

--- Array/new.py
+++ Array/new.py
@@ -1,21 +1,13 @@
 def twoSum(nums, target):
-    # Ek empty dictionary banayein jo value aur uske index ko store karegi
     seen = {}
-    
-    # Array ke har element par traverse karein
     for index, num in enumerate(nums):
-        # Hume kis number ki talaash hai?
         remaining = target - num
-      
-        if remaining in seen:
-            return [seen[remaining], index]
-            
-        # Agar nahi dikha, toh current number aur uska index dictionary mein daal dein
+        match_index = seen.get(remaining)
+        if match_index is not None:
+            return [match_index, index]
         seen[num] = index
-        
-    return []  # Agar koi pair na mile (waise question ke mutabik hamesha milega)
+    return []
 
-# Test the function
 nums = [2, 7, 11, 15]
 target = 9
-print(twoSum(nums, target))  # Output: [0, 1]
+print(twoSum(nums, target)) 
