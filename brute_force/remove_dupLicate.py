fd3f35ffef45c5589a0eb5dd20a6f8bc50cf52ca
Ayush Rusiya <166496159+ayushrusiya9@users.noreply.github.com>
2026-06-05 23:16:58 +0530

    Update remove_dupLicate.py

--- brute_force/remove_dupLicate.py
+++ brute_force/remove_dupLicate.py
@@ -1,13 +1,17 @@
 li = [1,2,2,2,3,3,3,4,5,5,5]
 
-def removeDuplicate(n):
-    # print(n)
-    n = set(n)
-    # print(remove_dup)
-    n = list(n)
-    # print(set_to_list)
-    n.sort()
-    return n
+# def removeDuplicate(n):
+#     # print(n)
+#     n = set(n)
+#     # print(remove_dup)
+#     n = list(n)
+#     # print(set_to_list)
+#     n.sort()
+#     return n
 
-ans = removeDuplicate(li)
-print(ans,"lenth:", len(ans))
+# ans = removeDuplicate(li)
+# print(ans,"lenth:", len(ans))
+
+def removeDuplicate(n):
+    pass
+    
